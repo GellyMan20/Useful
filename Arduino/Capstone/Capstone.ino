4d1f62412082256246062a59765a0577a3b868ca
GellyMan20 <76624109+GellyMan20@users.noreply.github.com>
2021-01-17 16:52:14 -0500

    Add files via upload

---
+++ Arduino/Capstone/Capstone.ino
@@ -0,0 +1,44 @@
+#include <AFMotor.h>
+AF_DCMotor motor1(1);
+
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+
+void setup() {
+   Serial.begin(9600); // Starting Serial Terminal
+   motor1.setSpeed(255);
+}
+
+void loop() {
+   long duration, inches, cm;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   inches = microsecondsToInches(duration);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(inches);
+   Serial.print("in, ");
+   Serial.println();
+   delay(100);
+
+   if(inches >= 2){
+   motor1.setSpeed(225);
+   }
+   else{
+   motor1.setSpeed(0);
+   }
+   motor1.run(FORWARD);
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
